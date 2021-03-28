c0ce274c3fb72b32a5598d6deb3f8e92ffb17a31
Alexis Morris <alexis417@icloud.com>
2021-03-28 01:33:33 -0500

    Worked more on game

--- scripts/tic-tac-toe.js
+++ scripts/tic-tac-toe.js
@@ -1,3 +1,67 @@
 var gameOver = false;
 var currentPlayer = 'X';
+var totalMove = 0;
 
+function placeMarker(spotId){
+    if(!gameOver){
+        var spot = document.getElementById(spotId);
+
+        if(spot.isEmpty){
+            spot.innerHTML = currentPlayer;
+        }
+    }
+}
+
+function currentPlayerWon(){
+    var s1 = document.getElementById("spot1");
+    var s2 = document.getElementById("spot2");
+    var s3 = document.getElementById("spot3");
+    var s4 = document.getElementById("spot4");
+    var s5 = document.getElementById("spot5");
+    var s6 = document.getElementById("spot6");
+    var s7 = document.getElementById("spot7");
+    var s8 = document.getElementById("spot8");
+    var s9 = document.getElementById("spot9");
+
+    if((s1.innerHTML === s2.innerHTML && s2.innerHTML === s3.innerHTML) 
+    || (s4.innerHTML === s5.innerHTML && s5.innerHTML === s6.innerHTML) 
+    || (s7.innerHTML === s8.innerHTML && s8.innerHTML === s9.innerHTML) 
+    || (s1.innerHTML === s4.innerHTML && s4.innerHTML === s7.innerHTML)
+    || (s2.innerHTML === s5.innerHTML && s5.innerHTML === s8.innerHTML)
+    || (s3.innerHTML === s6.innerHTML && s6.innerHTML === s9.innerHTML)
+    || (s1.innerHTML === s5.innerHTML && s5.innerHTML === s9.innerHTML)
+    || (s3.innerHTML === s5.innerHTML && s5.innerHTML === s7.innerHTML))
+    {
+        alert(s1.innerHTML);
+        alert("s1 value is " + s1.innerHTML)
+        gameOver = true;            
+    }
+    else{
+        gameOver = false;
+    }
+
+    if(!gameOver){
+        if(totalMove === 9){
+            gameOver = true;
+        }
+        else{
+            gameOver = false;
+        }
+    }
+}
+
+function updateGameStatus(){
+    var statusBoard = document.getElementById('status');
+
+    if(gameOver){
+        statusBoard.innerHTML = 'Player X won!';
+    }
+    else{
+        if(totalMove !== 9){
+            statusBoard.innerHTML = 'Player O it is your turn'
+        }
+        else{
+            statusBoard.innerHTML = 'It is a tie.'
+        }
+    }
+}
